c8b7ba450fce6966398eb7436a0776333cddafa6
Антон Матушкин <matushkin-2017@mail.ru>
2024-12-14 11:03:38 +0500

    chore: add models

---
+++ src/main/java/com/dlvb/graphqlmoviecatalogue/model/Review.java
@@ -0,0 +1,34 @@
+package com.dlvb.graphqlmoviecatalogue.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @ManyToOne(optional = false)
+    private Movie movie;
+
+}
